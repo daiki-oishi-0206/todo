0797df7e94e070d270647d301495837b22e2ed7c
oishi daiki <kanadai1122@icloud.com>
2026-04-24 04:48:51 +0900

    フォームスペースまで完了

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -7,26 +7,26 @@
 @section('content')
 <div class="todo">
     <!-- フォーム画面 -->
-    <div class="todo_form">
+    <div class="todo__form">
         <form action="/" method="post">
             @csrf
-            <input type="text" name="content">
-            <button type="submit">作成</button>
+            <input  class="todo__input" type="text" name="content">
+            <button class="todo__button" type="submit">作成</button>
         </form>
     </div>
 
     <!-- todo画面 -->
-     <div class="todo-list">
-        <h2 class="todo-list_title">Todo</h2>
+     <div class="todo__list">
+        <h3 class="todo__list--title">Todo</h3>
 
-        <ul class="todo-list_items">
-                <li class="todo-list_item">
-                    <span class="todo-list_content">
+        <ul class="todo__list--items">
+                <li class="todo__list--item">
+                    <span class="todo__list-content">
                         サンプルタスク
                     </span>
 
-                    <button class="todo-list_btn update">更新</button>
-                    <button class="todo-list_btn delete">削除</button>
+                    <button class="todo__list--btn update">更新</button>
+                    <button class="todo__list--btn delete">削除</button>
                 </li>
         </ul>
      </div>
