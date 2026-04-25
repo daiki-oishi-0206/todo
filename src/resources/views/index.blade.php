561071789158a7b2c305bd17fe813f73e9b4996b
oishi daiki <kanadai1122@icloud.com>
2026-04-26 03:55:43 +0900

    storeアクション追加

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -10,26 +10,30 @@
     <div class="todo__form">
         <form action="/" method="post">
             @csrf
-            <input  class="todo__input" type="text" name="content">
+            <input class="todo__input" type="text" name="content">
             <button class="todo__button" type="submit">作成</button>
         </form>
     </div>
 
     <!-- todo画面 -->
-     <div class="todo__list">
+    <div class="todo__list">
         <h3 class="todo__list--title">Todo</h3>
 
         <ul class="todo__list--items">
-                <li class="todo__list--item">
-                    <span class="todo__list-content">
-                        サンプルタスク
-                    </span>
-
+            @foreach($todos as $todo)
+            <li class="todo__list--item">
+                <span class="todo__list-content">
+                    {{ $todo->content }}
+                </span>
+                <div class="todo__actions">
                     <button class="todo__list--btn update">更新</button>
                     <button class="todo__list--btn delete">削除</button>
-                </li>
+                </div>
+            </li>
+            @endforeach
         </ul>
-     </div>
+
+    </div>
 
 </div>
 @endsection
